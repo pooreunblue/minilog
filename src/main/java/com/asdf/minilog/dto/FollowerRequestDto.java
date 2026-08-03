cf348e30b3df8fa5c7beebdcf5139e9b5b84d5e9
bsh <mjbsh02@mju.ac.kr>
2026-08-03 23:01:12 +0900

    feat: 팔로우 요청 DTO 추가

--- src/main/java/com/asdf/minilog/dto/FollowerRequestDto.java
+++ src/main/java/com/asdf/minilog/dto/FollowerRequestDto.java
@@ -1,4 +1,10 @@
 package com.asdf.minilog.dto;
 
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
 public class FollowerRequestDto {
+    @NonNull private Long followerId;
+    @NonNull private Long followeeId;
 }
