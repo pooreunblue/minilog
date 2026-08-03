b0313fb010dd0d496d31686b0448850eb0476548
bsh <mjbsh02@mju.ac.kr>
2026-08-03 22:58:22 +0900

    feat: 게시글 응답 DTO 추가

--- src/main/java/com/asdf/minilog/dto/ArticleResponseDto.java
+++ src/main/java/com/asdf/minilog/dto/ArticleResponseDto.java
@@ -1,4 +1,18 @@
 package com.asdf.minilog.dto;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class ArticleResponseDto {
+    @NonNull private Long articleId;
+    @NonNull private String content;
+    @NonNull private Long authorId;
+    @NonNull private String authorName;
+    @NonNull private LocalDateTime createdAt;
 }
