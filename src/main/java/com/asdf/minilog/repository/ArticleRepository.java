b7cbcbe785a9fc06ec7cc19f2710a14429c425e2
bsh <mjbsh02@mju.ac.kr>
2026-08-04 22:38:22 +0900

    feat: 게시글 저장소 구현 추가

--- src/main/java/com/asdf/minilog/repository/ArticleRepository.java
+++ src/main/java/com/asdf/minilog/repository/ArticleRepository.java
@@ -1,4 +1,20 @@
 package com.asdf.minilog.repository;
 
-public class ArticleRepository {
+import com.asdf.minilog.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+    List<Article> findAllByAuthorId(Long authorId);
+
+    @Query(
+            "SELECT a FROM Article a JOIN a.author u JOIN Follow f"
+            + " ON u.id = f.followee.id WHERE"
+            + " f.follower.id = :authorId ORDER BY a.createdAt DESC")
+    List<Article> findAllByFollowerId(@Param("authorId") Long authorId);
 }
