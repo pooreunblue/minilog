9a95eb1f742990ca0bc12e2bf3d8c01dea37411c
bsh <mjbsh02@mju.ac.kr>
2026-08-06 22:26:18 +0900

    feat: 게시글 서비스 계층 추가

--- src/main/java/com/asdf/minilog/service/ArticleService.java
+++ src/main/java/com/asdf/minilog/service/ArticleService.java
@@ -1,4 +1,27 @@
 package com.asdf.minilog.service;
 
+import com.asdf.minilog.dto.ArticleResponseDto;
+import com.asdf.minilog.entity.Article;
+import com.asdf.minilog.entity.User;
+import com.asdf.minilog.exception.UserNotFoundException;
+import com.asdf.minilog.repository.ArticleRepository;
+import com.asdf.minilog.repository.UserRepository;
+import com.asdf.minilog.util.EntityDtoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(isolation = Isolation.REPEATABLE_READ)
 public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public ArticleService(ArticleRepository articleRepository, UserRepository userRepository) {
+        this.articleRepository = articleRepository;
+        this.userRepository = userRepository;
+    }
 }
