c6e28c9ff6e70b331ee88fcf115441898fb08a0b
bsh <mjbsh02@mju.ac.kr>
2026-08-06 21:51:15 +0900

    feat: 사용자 서비스 계층 추가

--- src/main/java/com/asdf/minilog/service/UserService.java
+++ src/main/java/com/asdf/minilog/service/UserService.java
@@ -1,4 +1,26 @@
 package com.asdf.minilog.service;
 
+import com.asdf.minilog.dto.UserRequestDto;
+import com.asdf.minilog.dto.UserResponseDto;
+import com.asdf.minilog.entity.User;
+import com.asdf.minilog.repository.UserRepository;
+import com.asdf.minilog.util.EntityDtoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
